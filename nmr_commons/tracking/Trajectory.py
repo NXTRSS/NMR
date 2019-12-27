c6189f82676912163a7483671c7d48921b6484f5
NXTRSS <kamillo_j@yahoo.com>
2019-12-27 16:45:18 +0100

    Full class Trajectory

--- nmr_commons/tracking/Trajectory.py
+++ nmr_commons/tracking/Trajectory.py
@@ -1,5 +1,6 @@
 import csv
 import copy
+import numpy as np
 
 
 class Trajectory:
@@ -25,6 +26,22 @@ class Trajectory:
     def __len__(self):
         return len(self.list)
 
+    def get_x(self, position):
+        if self[position] is not None:
+            return self[position][0]
+        return None
+
+    def get_y(self, position):
+        if self[position] is not None:
+            return self[position][1]
+        return None
+
+    def get_vector(self, position):
+        for i, point in enumerate(self[position:-1]):
+            if point is not None and self[position + i + 1] is not None:
+                return position + i
+        return None
+
     def add_point(self, point, position=None):
         if position is not None:
             self.list[position] = tuple(point)
@@ -41,7 +58,11 @@ class Trajectory:
         return self.list[position]  # I'm not sure if this is necessary - getitem doesn't do the same?
 
     def remove_point(self, position):
-        self.list[position] = None
+        if self[:position] == [None] * len(self[:position]) and self[position + 1:] == [None] * len(
+                self[position + 1:]):
+            raise ValueError('You can not remove all elements from trajectory')
+        else:
+            self.list[position] = None
 
     def remove_points(self, position_list):
         for position in position_list:
@@ -58,38 +79,190 @@ class Trajectory:
             writer = csv.writer(output, lineterminator="\n", delimiter=';')
             writer.writerow(self.list)
 
-    def get_next_point(self, point):
+    def get_next_point(self, position):
         index = None
-        for i in range(point + 1, len(self.list)):
-            if self.list[i] is not None:
+        for i in range(position + 1, len(self)):
+            if self[i] is not None:
                 index = i
                 break
         return index
 
-    def get_distances_old(self):
-        distance = list()
-        for i in range(len(self.list) - 1):
-            if self.list[i] is not None:
+    def get_distances(self):
+        distances = [None] * (len(self) - 1)
+        for i, point in enumerate(self[:-1]):
+            if self[i] is not None:
                 k = i + 1
-                while k != len(self.list) - 1 and self.list[k] is None:
-                    k += 1
-                if k == len(self.list) - 1 and self.list[k] is None:
-                    return distance
+                if self[k] is not None:
+                    distances[i] = ((point[0] - self.get_x(k)),
+                                    (point[1] - self.get_y(k)))
                 else:
-                    for j in range(i, k):
-                        distance.append([(self.list[i][0] - self.list[k][0]) / (k - i),
-                                         (self.list[i][1] - self.list[k][1]) / (k - i)])
-        return distance
+                    while k != (len(self) - 1) and self[k] is None:
+                        k += 1
+                    if k == len(self) - 1 and self[k] is None:
+                        return distances
+                    else:
+                        for j in range(i, k):
+                            distances[j] = ((point[0] - self.get_x(k)) / (k - i),
+                                            (point[1] - self.get_y(k)) / (k - i))
+        return distances
 
-    # def get_distances(self):
-    #     distances = [None]*(len(self)-1)
-    #     for i, point in enumerate(self[:-1]):
-    #         if self[i] is not None:
-    #             k = i + 1
-    #             while k != (len(self)-1) and self[k] is None:
-    #                 k += 1
-    #             if k == len(self) - 1 and self[k] is None:
-    #                 return distances
-    #             else:
-    #                 for j in range(i, k):
-    #                     distances[j] =
+    def get_distances_sep(self):
+        distances = list()
+        for i, point in enumerate(self[:-1]):
+            if self.get_vector(i) == i:
+                distances.append(tuple((point[0] - self.get_x(i + 1), point[1] - self.get_y(i + 1))))
+        return distances
+
+    @staticmethod
+    def unit_vector(vector):
+        """ Returns the unit vector of the vector.  """
+        return vector / np.linalg.norm(vector)
+
+    def get_angles(self):
+        angles = []
+        for i in range(len(self) - 2):
+            idx_first_vec = self.get_vector(i)
+            if idx_first_vec is not None:
+                a = self[idx_first_vec]
+                b = self[idx_first_vec + 1]
+
+                idx_second_vec = self.get_vector(idx_first_vec + 1)
+                if idx_second_vec is not None:
+                    c = self[idx_second_vec]
+                    d = self[idx_second_vec + 1]
+                    v1 = self.unit_vector(tuple([x1 - x2 for x1, x2 in zip(b, a)]))
+                    v2 = self.unit_vector(tuple([x1 - x2 for x1, x2 in zip(d, c)]))
+                    angles.append(np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0)))
+        return angles
+
+    def get_all_x(self, with_none_points=True):
+        if with_none_points:
+            return [point[0] if point is not None else None for point in self]
+        else:
+            return [point[0] for point in self if point is not None]
+
+    def get_all_y(self, with_none_points=True):
+        if with_none_points:
+            return [point[1] if point is not None else None for point in self]
+        else:
+            return [point[1] for point in self if point is not None]
+
+    def get_mean(self):
+        xn = self.get_all_x(with_none_points=False)
+        yn = self.get_all_y(with_none_points=False)
+        return [np.mean(xn), np.mean(yn)]
+
+    def can_merge(self, trj):
+        min_idx = min(len(self), len(trj))
+        for i in range(min_idx):
+            if (self[i] is not None) and (trj[i] is not None):
+                return False
+        return True
+
+    def merge(self, trj):
+        for i in range(min(len(self), len(trj))):
+            if trj[i] is not None:
+                self.add_point(trj[i], i)
+
+    def get_first_last(self):
+        not_none_idxs = [i for i in range(len(self)) if self[i] is not None]
+        first_idx = not_none_idxs[0]
+        last_idx = not_none_idxs[-1]
+        first_point = self[first_idx]
+        last_point = self[last_idx]
+
+        return first_idx, last_idx, first_point, last_point
+
+    def get_not_none_count(self):
+        return sum(point is not None for point in self)
+
+    def get_path_ratio(self, x_div=35.0, y_div=6.0):
+        ratio = 1
+        sum_dist = 0
+        first_last_dist = 0
+        if self.get_not_none_count() > 2:
+            (first, last, first_point, last_point) = self.get_first_last()
+            first_last_dist = np.sqrt(((first_point[0] - last_point[0]) / x_div) ** 2 +
+                                      ((first_point[1] - last_point[1]) / y_div) ** 2)
+            if first_last_dist > 0:
+                for i in range(first + 1, last + 1):
+                    if self[i] is not None:
+                        last_point = self[i]
+                        sum_dist = sum_dist + np.sqrt(((first_point[0] - last_point[0]) / x_div) ** 2
+                                                      + ((first_point[1] - last_point[1]) / y_div) ** 2)
+                        first_point = last_point
+                ratio = float(sum_dist) / float(first_last_dist)
+        return ratio, sum_dist, first_last_dist
+
+    def get_stats(self, x_div, y_div):
+
+        v_mod_mean = 0
+        v_mod_std = 0
+        angle_mean = 0
+        angle_std = 0
+        v_s = []
+        angles = []
+        if (self.get_not_none_count() > 2):
+            (first, last, first_point, last_point) = self.get_first_last()
+            div = 1.0
+            for k in range(first + 1, last + 1):
+                if self.list[k] is not None:
+                    last_point = self.list[k];
+                    dist = np.sqrt(((first_point[0] - last_point[0]) / x_div) ** 2 +
+                                ((first_point[1] - last_point[1]) / y_div) ** 2) / div
+                    angle = np.abs(np.arctan2(((first_point[1] - last_point[1]) / y_div),
+                                              ((first_point[0] - last_point[0]) / x_div)))
+                    v_s.append(dist)
+                    angles.append(angle)
+                    first_point = last_point
+                    div = 1.0
+                else:
+                    div += 1.0
+
+            v_mod_mean = np.mean(v_s)
+            v_mod_std = np.std(v_s)
+            angle_mean = np.mean(angles)
+            angle_std = np.std(angles)
+        return v_mod_mean, v_mod_std, angle_mean, angle_std, v_s, angles
+
+    def is_correct(self, x_div=35.0, y_div=6.0):
+        v_s = []
+        angles = []
+        if self.get_not_none_count() > 2:
+            (first, last, first_point, last_point) = self.get_first_last()
+            div = 1.0
+            for k in range(first + 1, last + 1):
+                if self[k] is not None:
+                    last_point = self[k]
+                    d = np.sqrt(((first_point[0] - last_point[0]) / x_div) ** 2 +
+                                ((first_point[1] - last_point[1]) / y_div) ** 2) / div
+                    en = np.abs(np.arctan2(((first_point[1] - last_point[1]) / y_div),
+                                           ((first_point[0] - last_point[0]) / x_div)))
+                    if len(v_s) >= 2:
+                        if d > 0.1:
+                            if np.abs(v_s[-2] - d) > 1:  # or (np.abs(engles[-2]-en)>0.8)):
+                                return False
+
+                    v_s.append(d)
+                    angles.append(en)
+                    first_point = last_point
+                    div = 1.0
+                else:
+                    div += 1.0
+        return True
+
+    def init_nones(self, K):
+        for k in range(K):
+            self.add_point(None)
+
+    @staticmethod
+    def read_from_csv(row):
+        line = Trajectory()
+        for elem in row:
+            if elem != '':
+                corrected_elem = eval(elem)
+                line.add_point(corrected_elem)
+            else:
+                line.add_point(None)
+
+        return line
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,34 @@
 import unittest
 from nmr_commons.tracking.Trajectory import Trajectory
 
+
 class TestTrajectoryMethods(unittest.TestCase):
     def setUp(self) -> None:
-        self.trajectory = Trajectory([(1, 1), (2, 2), None, None, (4, 4), (5, 5)])
+        self.trajectory = Trajectory([(1, 1), (2, 2), None, None, (8, 8), (9, 10.5)])
+        self.trajectory_complement_short = Trajectory([None, None, (3, 3), None, None])
+        self.trajectory_complement_long = Trajectory([None, None, (3, 3), (4, 4), None, None, (10, 13)])
+        self.trajectory_angle = Trajectory([(1, 1), (2, 2), None, (3, 3), (4, 2)])
+
+    def test_get_x_method_normal_case(self):
+        self.assertEqual(self.trajectory.get_x(5), 9)
+
+    def test_get_x_method_none_case(self):
+        self.assertEqual(self.trajectory.get_x(3), None)
+
+    def test_get_y_method_normal_case(self):
+        self.assertEqual(self.trajectory.get_y(5), 10.5)
+
+    def test_get_y_method_none_case(self):
+        self.assertEqual(self.trajectory.get_x(3), None)
+
+    def test_get_vector_method_normal_case(self):
+        self.assertEqual(self.trajectory.get_vector(0), 0)
+
+    def test_get_vector_method_none_between(self):
+        self.assertEqual(self.trajectory.get_vector(1), 4)
+
+    def test_get_vector_method_last_point(self):
+        self.assertEqual(self.trajectory.get_vector(5), None)
 
     def test_next_point_method_normal_case(self):
         self.assertEqual(self.trajectory.get_next_point(0), 1)
@@ -16,6 +41,67 @@ class TestTrajectoryMethods(unittest.TestCase):
 
     def test_next_point_method_last_point(self):
         self.assertEqual(self.trajectory.get_next_point(5), None)
+
+    def test_get_distances(self):
+        self.assertEqual(self.trajectory.get_distances(),
+                         [(-1 * x[0], -1 * x[1]) for x in [(1, 1), (2, 2), (2, 2), (2, 2), (1, 2.5)]])
+
+    def test_get_distances_sep(self):
+        self.assertEqual(self.trajectory.get_distances_sep(),
+                         [(-1 * x[0], -1 * x[1]) for x in [(1, 1), (1, 2.5)]])
+
+    def test_get_angles(self):
+        self.assertAlmostEqual(self.trajectory_angle.get_angles()[0], 1.57, places=2)
+
+    def test_get_angles(self):
+        self.assertAlmostEqual(self.trajectory.get_angles()[0], 0.4, places=2)
+
+    def test_get_all_x(self):
+        self.assertEqual(self.trajectory.get_all_x(), [1, 2, None, None, 8, 9])
+
+    def test_get_all_x(self):
+        self.assertEqual(self.trajectory.get_all_x(with_None_points=False), [1, 2, 8, 9])
+
+    def test_get_all_y(self):
+        self.assertEqual(self.trajectory.get_all_y(), [1, 2, None, None, 8, 10.5])
+
+    def test_get_mean(self):
+        self.assertEqual(self.trajectory.get_mean(), [5, 5.375])
+
+    def test_can_marge_true_short(self):
+        self.assertEqual(self.trajectory.can_merge(self.trajectory_complement_short), True)
+
+    def test_can_marge_true_long(self):
+        self.assertEqual(self.trajectory.can_merge(self.trajectory_complement_long), True)
+
+    def test_can_marge_false(self):
+        self.assertEqual(self.trajectory.can_merge(self.trajectory_angle), False)
+
+    def test_marge_short(self):
+        self.trajectory.merge(self.trajectory_complement_short)
+        self.assertEqual(self.trajectory[:], [(1, 1), (2, 2), (3, 3), None, (8, 8), (9, 10.5)])
+
+    def test_marge_long(self):
+        self.trajectory.merge(self.trajectory_complement_long)
+        self.assertEqual(self.trajectory[:], [(1, 1), (2, 2), (3, 3), (4, 4), (8, 8), (9, 10.5)])
+
+    def test_get_first_last_normal_calse(self):
+        self.assertEqual(self.trajectory.get_first_last(), (0, 5, (1, 1), (9, 10.5)))
+
+    def test_get_first_last_none_in_the_beggining(self):
+        self.assertEqual(self.trajectory_complement_long.get_first_last(), (2, 6, (3, 3), (10, 13)))
+
+    def test_get_first_last_only_one_element(self):
+        self.assertEqual(self.trajectory_complement_short.get_first_last(), (2, 2, (3, 3), (3, 3)))
+
+    def test_get_not_none_count_normal(self):
+        self.assertEqual(self.trajectory.get_not_none_count(), 4)
+
+    def test_get_path_ratio_sum_dist(self):
+        self.assertAlmostEqual(self.trajectory.get_path_ratio()[1], 1.6013, places=4)
+
+    def test_get_path_ratio_first_last_dist(self):
+        self.assertAlmostEqual(self.trajectory.get_path_ratio()[2], 1.5997, places=4)
 
 if __name__ == '__main__':
     unittest.main()
